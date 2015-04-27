5355146e156c8d87db09c73ae9eda46baed2c143
Ryan Swapp <ryancswapp@gmail.com>
2015-04-27 17:13:52 -0600

    Updated package.js with version numbers

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'ryanswapp:datetime-picker',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'A simple package for adding a generic datetime picker',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ryanswapp/datetime-picker.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('jquery', 'client');
+  api.use('jquery@1.11.3_2', 'client');
   api.addFiles('datetime-picker.css', 'client');
   api.addFiles('datetime-picker.js', 'client');
 });
